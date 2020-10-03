8f04057d9ad867b98ee58963484d54f5ea0b2b54
Ali serin <aliserin@Ali-MacBook-Air.local>
2020-10-03 19:14:19 +0300

    home screen animation

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,68 +1,131 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react'
-import { Image, Text, StyleSheet, StatusBar } from 'react-native'
+import {
+  Animated,
+  Text,
+  StyleSheet,
+  StatusBar,
+  ImageBackground,
+  Dimensions,
+  Easing
+} from 'react-native'
 
 import Button from '../components/button'
 import Box from '../components/box'
+//import bg from '../assets/bg'
+
+const WINDOW_WIDTH = Dimensions.get('window').width
+const WINDOW_HEIGHT = Dimensions.get('window').height
 
 function HomeScreen(props) {
+  let yValue = new Animated.Value(0)
+  let scaleValue = new Animated.Value(0)
+  let enteranceVal = new Animated.Value(0)
+
+  React.useEffect(() => {
+    const moveLR = () => {
+      Animated.sequence([
+        Animated.timing(enteranceVal, {
+          toValue: 1,
+          duration: 500,
+          easing: Easing.linear,
+          useNativeDriver: true
+        }),
+        Animated.timing(yValue, {
+          delay: 700,
+          toValue: -200,
+          duration: 1000, // the duration of the animation
+          easing: Easing.linear, // the style of animation
+          useNativeDriver: true
+        }),
+        Animated.timing(scaleValue, {
+          toValue: 1,
+          duration: 500,
+          easing: Easing.linear,
+          useNativeDriver: true
+        })
+      ]).start()
+    }
+    moveLR()
+  }, [enteranceVal, scaleValue, yValue])
   return (
     <Box style={styles.homePage}>
-      <StatusBar barStyle="light-content" />
-      <Box>
-        <Image style={styles.welcome} source={require('../assets/logo.png')} />
-        <Box style={{ marginTop: 400 }}>
-          <Button
-            alignSelf="flex-start"
-            ml={265}
-            onPress={() => {
-              props.navigation.navigate('Challenges')
+      <ImageBackground
+        source={require('../assets/bg3.jpg')}
+        style={styles.image}
+      >
+        <StatusBar barStyle="light-content" />
+        <Box>
+          <Animated.Image
+            style={{
+              alignSelf: 'center',
+              marginTop: 100,
+              transform: [{ translateY: yValue }],
+              opacity: enteranceVal
             }}
-          >
-            <Text
-              style={[
-                styles.buttonText,
-                { color: '#ED36BA', fontWeight: 'bold' }
-              ]}
+            source={require('../assets/logo.png')}
+          />
+
+          <Box>
+            <Animated.View
+              style={{
+                marginTop: -70,
+                opacity: scaleValue
+                /* transform: [{ scale: scaleValue }] */
+              }}
             >
-              Play
-            </Text>
-          </Button>
-          <Button
-            alignSelf="flex-start"
-            ml={265}
-            mt={26}
-            onPress={() => {
-              props.navigation.navigate('MenuBar')
-            }}
-          >
-            <Text
-              style={[
-                styles.buttonText,
-                { color: '#3B4AE6', fontWeight: 'bold' }
-              ]}
-            >
-              Menu
-            </Text>
-          </Button>
+              <Button
+                style={{
+                  alignSelf: 'center',
+                  //borderWidth: 1,
+                  backgroundColor: '#ED36BA',
+                  width: WINDOW_WIDTH / 2,
+                  borderRadius: 20,
+                  justifyContent: 'center'
+                }}
+                onPress={() => {
+                  props.navigation.navigate('Challenges')
+                }}
+              >
+                <Text style={[styles.buttonText]}>Play</Text>
+              </Button>
+              <Button
+                style={{ alignSelf: 'center' }}
+                onPress={() => {
+                  props.navigation.navigate('MenuBar')
+                }}
+              >
+                <Text
+                  style={[
+                    styles.buttonText,
+                    { color: '#3B4AE6', fontWeight: 'bold' }
+                  ]}
+                >
+                  Menu
+                </Text>
+              </Button>
+            </Animated.View>
+          </Box>
         </Box>
-      </Box>
+      </ImageBackground>
     </Box>
   )
 }
 
 const styles = StyleSheet.create({
-  welcome: {
-    alignSelf: 'center',
-    marginTop: 150
-  },
   homePage: {
-    flex: 1,
-    backgroundColor: '#1F252D'
+    flex: 1
   },
   buttonText: {
-    fontSize: 28,
-    color: 'white'
+    fontSize: 36,
+    color: 'white',
+    fontFamily: 'Starjedi',
+    marginTop: -5
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
   }
 })
 
